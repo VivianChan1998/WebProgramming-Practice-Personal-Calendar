97baa5b036a4ce677c7c68004860773e984fbcb6
Vivian <b05901179@ntu.edu.tw>
2020-09-30 10:47:39 +0800

    input

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 var cells = document.getElementsByClassName("date")
+var input = document.getElementById("cal-input")
+var setText = document.getElementById("cal-setText")
+
+setText.addEventListener('click', () => {
+    var sel = document.getElementsByClassName('selected')[0]
+    if(sel.childElementCount){
+        sel.children[0].textContent = input.value
+    }
+    else{
+        var node = document.createElement("span")
+        var text = document.createTextNode(input.value)
+        node.appendChild(text)
+        sel.appendChild(node)
+    }
+}, false)
 
 for(var i=0; i < cells.length; ++i){
     cells[i].addEventListener('click', clickCell, false)
@@ -9,4 +24,6 @@ function clickCell(e) {
         cells[i].classList.remove('selected');
     }
     this.classList.add('selected');
+    if(this.children.length) input.value = this.children[0].textContent
+    else input.value = ''
 }
