abcdedba7651040bf8639aaffea14452eb9c9a55
Vivian <b05901179@ntu.edu.tw>
2020-09-30 15:35:57 +0800

    code clean up

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var cells = document.getElementsByClassName("date")
 var input = document.getElementById("cal-input")
-input.value = ''
 var color = document.getElementById("cal-color")
+var button = document.getElementById("cal-button")
+input.value = ''
 color.value = '#b0b0b0'
-var setText = document.getElementById("cal-setText")
 
-setText.addEventListener('click', () => {
+button.addEventListener('click', () => {
     var sel = document.getElementsByClassName('selected')[0]
     if(sel.childElementCount){
         sel.children[0].textContent = input.value
@@ -33,11 +33,9 @@ function clickCell(e) {
     else input.value = ''
 }
 
-
 var themeButton = document.getElementsByClassName("ChooseTheme")
 for(var i=0; i<themeButton.length; ++i) {
     themeButton[i].addEventListener('click', e => {
-        var body = document.body
-        body.setAttribute('class', e.target.id)
+        document.body.setAttribute('class', e.target.id)
     }, false)
 }
