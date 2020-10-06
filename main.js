f8bcb52631b8217e9ff41af5bee5e1837ba0028a
Vivian <b05901179@ntu.edu.tw>
2020-10-06 17:45:02 +0800

    .

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var cells = document.getElementsByClassName("date")
 var input = document.getElementById("cal-input")
 var color = document.getElementById("cal-color")
 var button = document.getElementById("cal-button")
+var prev_sel = cells[0]
 input.value = ''
 color.value = '#b0b0b0'
 
 button.addEventListener('click', setTextToCell, false)
 input.addEventListener('keydown', e => { if(e.key === "Enter") setTextToCell() }, false)
+for(var i=0; i < cells.length; ++i){
+    cells[i].addEventListener('click', clickCell, false)
+}
 
 function setTextToCell() {
     var sel = document.getElementsByClassName('selected')[0]
@@ -23,19 +27,30 @@ function setTextToCell() {
     }
 }
 
-for(var i=0; i < cells.length; ++i){
-    cells[i].addEventListener('click', clickCell, false)
-}
-
 function clickCell(e) {
-    for(var i = 0; i < cells.length; i++) {
-        cells[i].classList.remove('selected');
-    }
+    prev_sel.classList.remove('selected');
+    prev_sel = this
     this.classList.add('selected');
-    if(this.children.length) input.value = this.children[0].textContent
-    else input.value = ''
+    if(this.children.length) {
+        input.value = this.children[0].textContent
+        color.value = rgb2Hex(this.children[0].style.color)
+    }    
+    else {
+        input.value = ''
+        color.value = '#b0b0b0'
+    }
 }
 
+//Additional tool for retrieving the color that was set to text
+function rgb2Hex(arr){
+    var str = arr.split("(")[1]
+    str = str.split(")")[0]
+    str = str.split(",")
+    var rgb = {r: parseInt(str[0]), g: parseInt(str[1]), b: parseInt(str[2])}
+    return '#' + (rgb.r).toString(16) + (rgb.g).toString(16) + (rgb.b).toString(16)
+}
+
+//Theme
 var themeButton = document.getElementsByClassName("ChooseTheme")
 for(var i=0; i<themeButton.length; ++i) {
     themeButton[i].addEventListener('click', e => {
